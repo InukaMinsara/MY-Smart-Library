61b2dbeea2284729eba73c28c56565c675c3d45e
GitHub User <user@github.com>
2026-08-12 20:45:45 +0530

    feat: add Electron desktop app wrapper

---
+++ electron-app/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
+    title: "Smart Library Pro",
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  // Netlify ලින්ක් එක Load කිරීම
+  win.loadURL('https://smartlibrary02.netlify.app/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
